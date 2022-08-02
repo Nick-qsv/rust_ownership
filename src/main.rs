4515d109d83f2ebdcab778f25ac5a540ddc03062
Nick Baez <w0m85k27@protonmail.com>
2022-08-01 22:58:58 -0400

    second commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let v = vec![1,2,3];
+    //V owns the memory stored in the vector
+    //vec data is on the heap though
+
+    // let v2 = v;
+    //only a single variable can own the memory at any time.
+    //copying a pointer, so you have to two pointers to the same content
+    //this can introduce a race condition.  so only one pointer allowed.
+
+    let u = 1;
+    let u2 = u;
+
+    println!("u = {}", u);
+    //works for primitives, because we can copy, it's stored on the stack.  no pointers to data
+    
+    let print_vector = |x:Vec<i32>| -> Vec<i32>{
+        println!("{:?}", x);
+        x
+    };
+    //borrow and then return the vector before the end.  you can printline it and then return it
+
+    let vv = print_vector(v);
+    println!("{}", vv[0]);
+
 }
